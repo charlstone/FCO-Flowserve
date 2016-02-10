7f438db6cc7ea9ff8c2f0ac6c8b93c6ad0dd9d50
Marcosmemo <marcoshernandezmora@outlook.com>
2016-02-10 11:53:02 -0600

    Source Sites and Dates for filters

--- Flowserve-FCO/Scripts/flowersve-FCO.js
+++ Flowserve-FCO/Scripts/flowersve-FCO.js
@@ -7,42 +7,117 @@ var columns = [];
 var siteLoad = [];
 $(document).ready(function () {
 
-    //FCO = [
-    //    {
-    //        FCOID: 1,
-    //        WeekDate: "02/02/2016",
-    //        SiteID: "14",
-    //        IDOG: "5",
-    //        OGName: "OG China",
-    //        IDRegion: "6",
-    //        RegionName: "Region Americas",
-    //        ID: "14|5|6",
-    //        Total: 18.0000
-    //    },
-    //    {
-    //        FCOID: 2,
-    //        WeekDate: "02/02/2016",
-    //        SiteID: "10",
-    //        IDOG: "14",
-    //        OGName: "OG Process",
-    //        IDRegion: "16",
-    //        RegionName: "Region Test",
-    //        ID: "10|14|16",
-    //        Total: 20.0000
-    //    }
-    //];
+    FCO = [
+        {
+            FCOID: 1,
+            WeekDate: "02/02/2016",
+            SiteID: "14",
+            SiteName: "Site 14",
+            IDOG: "5",
+            OGName: "OG China",
+            IDRegion: "6",
+            RegionName: "Region Americas",
+            ID: "14|5|6",
+            Total: 18.0000
+        },
+        {
+            FCOID: 2,
+            WeekDate: "02/02/2016",
+            SiteID: "10",
+            SiteName: "Site 10",
+            IDOG: "14",
+            OGName: "OG Process",
+            IDRegion: "16",
+            RegionName: "Region Test",
+            ID: "10|14|16",
+            Total: 20.0000
+        }
+    ];
 
-    ////local testing values 
-            
-    //userPermision = [
-    //            { "IDSite": 4, "SiteName": "test 2", "IDOG": 15, "OGName": "OG China", "IDRegion": 0, "RegionName": "" },
-    //            { "IDSite": 0, "SiteName": "test 2", "IDOG": 15, "OGName": "OG China", "IDRegion": 0, "RegionName": "" },
-    //            { "IDSite": 10, "SiteName": "Site Cookeville", "IDOG": 14, "OGName": "OG Process", "IDRegion": 16, "RegionName": "Region Americas", "ID": "10|14|16" },
-    //            { "IDSite": 13, "SiteName": "Test New", "IDOG": 13, "OGName": "OG Test", "IDRegion": 13, "RegionName": "Region Test", "ID": "13|13|13" }
-    //];
+    //local testing values 
+
+    userPermision = [
+                { "IDSite": 4, "SiteName": "test 2", "IDOG": 15, "OGName": "OG China", "IDRegion": 0, "RegionName": "" },
+                { "IDSite": 0, "SiteName": "test 2", "IDOG": 15, "OGName": "OG China", "IDRegion": 0, "RegionName": "" },
+                { "IDSite": 10, "SiteName": "Site Cookeville", "IDOG": 14, "OGName": "OG Process", "IDRegion": 16, "RegionName": "Region Americas", "ID": "10|14|16" },
+                { "IDSite": 13, "SiteName": "Test New", "IDOG": 13, "OGName": "OG Test", "IDRegion": 13, "RegionName": "Region Test", "ID": "13|13|13" }
+    ];
     //SHAREPOINT, DEPLOY VERSION
-    FCO = $(".hdnBookings").text() != "" ? JSON.parse($(".hdnBookings").text()) : [];
-    userPermision = JSON.parse($(".hdnSites").text());
+    //FCO = $(".hdnBookings").text() != "" ? JSON.parse($(".hdnBookings").text()) : [];
+    //userPermision = JSON.parse($(".hdnSites").text());
+    var sourceSites = [];
+    function createSourceSitesDropDown() {
+        var obj;
+        for (var iFCO = 0; iFCO < FCO.length; iFCO++) {
+            obj = {};
+            obj.value = FCO[iFCO].ID;
+            obj.text = FCO[iFCO].SiteName;
+            sourceSites.push(obj);
+        }
+    }
+
+    function closest_friday() {
+        var today = new Date(), friday, day, closest;
+
+        if (today.getDay() == 5) {
+            if (today.getHours() < 22) {
+                return today;
+            }
+        } else {
+            day = today.getDay();
+            friday = today.getDate() - day + (day === 0 ? -6 : 5);
+        }
+        closest = new Date(today.setDate(friday));
+
+        return ((closest.getMonth() + 1) > 9 ? (closest.getMonth() + 1) : "0" + (closest.getMonth() + 1)) + "/" + (closest.getDate() > 9 ? (closest.getDate()) : "0" + closest.getDate()) + "/" + closest.getFullYear();
+    }
+
+    var sourceDates = [];
+    function createSourceDatesDropDown() {
+        var actualDate = closest_friday();
+        sourceDates.push({"value":actualDate,"text":actualDate});
+        var actualYear = parseInt(actualDate.split("/")[2]);
+        var actualMonth = parseInt(actualDate.split("/")[0]);
+        var actualDay = parseInt(actualDate.split("/")[1]);
+
+        var x = new Date();
+        //set the actual year
+        x.setFullYear(actualYear, 01, 01);
+        //set the actual date
+        var y = new Date();
+        y.setFullYear(actualYear, actualDay, actualMonth);
+
+        var j = 1;
+        var count = 0;
+
+        //getting the all fridays in a financial year
+        var obj;
+        for (var i = 0; x < y; i += j) {
+            if (actualYear == x.getFullYear()) {
+                if (x.getDay() == 5) {
+                    obj = {}
+                    obj.value = ((x.getMonth() + 1) > 9 ? (x.getMonth() + 1) : "0" + (x.getMonth() + 1)) + "/" + ((x.getDate() ) > 9 ? (x.getDate() ) : "0" + (x.getDate() )) + "/" + x.getFullYear();
+                    obj.text = ((x.getMonth() + 1) > 9 ? (x.getMonth() + 1) : "0" + (x.getMonth() + 1)) + "/" + ((x.getDate() ) > 9 ? (x.getDate() ) : "0" + (x.getDate() )) + "/" + x.getFullYear();
+
+                    sourceDates.push(obj);
+                    //document.write("Date : " + x.getFullYear() + "/" + x.getDate() + "/"
+                    //        + (x.getMonth() + 1) + "<br>");
+                    x = new Date(x.getTime() - (7 * 24 * 60 * 60 * 1000));
+                    j = 7;
+                    count++;
+                } else {
+                    j = 1;
+                    x = new Date(x.getTime() + (24 * 60 * 60 * 1000));
+                }
+            }
+            else {
+                break;
+            }
+        }
+    }
+
+    createSourceSitesDropDown();
+    createSourceDatesDropDown();
 
     function mergeFCOwithPermission(permissions) {
         var iPermission = -1;
@@ -89,7 +164,7 @@ $(document).ready(function () {
         }
         return index;
     }
-    
+
     $(userPermision).each(function () {
         this.ID = (this.IDSite).toString() + "|" + (this.IDOG).toString() + "|" + (this.IDRegion).toString();
         if (this.IDSite > 0) {
@@ -102,15 +177,14 @@ $(document).ready(function () {
 
     FCO = mergeFCOwithPermission(userPermision);
 
-    if (userPermision[0].IsAdmin)
-    {
+    if (userPermision[0].IsAdmin) {
         fields = {
             model: {
                 id: "FCOID",
                 fields: {
                     FCOID: { editable: false, nullable: true },
-                    OGName: { field: "OGName", type: "string", editable: false},
-                    RegionName: { field: "RegionName", type: "string",editable: false },
+                    OGName: { field: "OGName", type: "string", editable: false },
+                    RegionName: { field: "RegionName", type: "string", editable: false },
                     ID: { field: "ID", type: "string", defaultValue: siteLoad[0].value, validation: { required: true, min: 0 }, editable: false, nullable: false },
                     WeekDate: {
                         type: "date", defaultValue: setFridayInWeek(),
@@ -150,8 +224,7 @@ $(document).ready(function () {
                 { command: ["edit"], title: "&nbsp;", width: "200px" }
         ];
     }
-    else
-    {
+    else {
         fields = {
             model: {
                 id: "FCOID",
@@ -194,8 +267,8 @@ $(document).ready(function () {
                 { command: ["edit"], title: "&nbsp;", width: "200px" }
         ];
     }
-        
-        
+
+
 
     // custom logic start
 
